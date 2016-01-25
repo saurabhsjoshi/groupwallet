e98a3ff2a6c81545b901ecf84e82fbaa05029330
Saurabh Joshi <battlex2010@yahoo.com>
2016-01-25 01:07:38 -0500

    Fixed indentation

--- models.go
+++ models.go
@@ -4,12 +4,12 @@ import "time"
 
 // User model in DB
 type User struct {
-	ID int    			`json:"id"`
-	Name string     	`json:"name"`
-	Email string    	`json:"email"`
-	Password string 	`json:"-"`
-	Token string        `json:"token"`
-	CreatedOn time.Time `json:"createdOn"`
+	ID int				`json:"id"`
+	Name string			`json:"name"`
+	Email string		`json:"email"`
+	Password string		`json:"-"`
+	Token string		`json:"token"`
+	CreatedOn time.Time	`json:"createdOn"`
 }
 
 // Slice of users
@@ -17,12 +17,12 @@ type Users [] User
 
 //Item model in DB
 type Item struct {
-	ID int            	`json:"id"`
-	Owner int           `json:"owner"`
-	Place string        `json:"place"`
-	Extra string        `json:"extra"`
-	Price float32       `json:"price"`
-	CreatedOn time.Time `json:"createdOn"`
+	ID int				`json:"id"`
+	Owner int			`json:"owner"`
+	Place string		`json:"place"`
+	Extra string		`json:"extra"`
+	Price float32		`json:"price"`
+	CreatedOn time.Time	`json:"createdOn"`
 }
 
 // Slice of items
