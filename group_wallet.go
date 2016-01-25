14c0b30af291777f4e1cb847b2bd0d8df10da6d8
Saurabh Joshi <battlex2010@yahoo.com>
2016-01-24 21:25:48 -0500

    Added register route

--- group_wallet.go
+++ group_wallet.go
@@ -3,16 +3,9 @@ package main
 import (
 	"log"
 	"net/http"
-	"github.com/gorilla/mux"
 )
 
 func main() {
-	router := mux.NewRouter().StrictSlash(true)
-	router.HandleFunc("/", Index)
+	router := NewRouter()
 	log.Fatal(http.ListenAndServe(":8080", router))
-
-}
-
-func Index(w http.ResponseWriter, r *http.Request){
-	RegisterUser(w, r)
 }
