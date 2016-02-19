ea2ed8f5d678aa8bf9981dbc63f0eea48233971e
Saurabh Joshi <battlex2010@yahoo.com>
2016-02-18 20:31:57 -0500

    Refactored

---
+++ user.go
@@ -0,0 +1,16 @@
+package main
+
+import "time"
+
+// User model in DB
+type User struct {
+	ID int64			`json:"id"`
+	Name string			`json:"name"`
+	Email string		`json:"email"`
+	Password string		`json:"-"`
+	Token string		`json:"token"`
+	CreatedOn time.Time	`json:"createdOn"`
+}
+
+// Slice of users
+type Users [] User
